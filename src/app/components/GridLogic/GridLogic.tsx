6affbe05e8924955c625f1ef471140631b48cdea
Nathaniel Miller <dukeoflaser@gmail.com>
2021-06-09 21:11:46 -0700

    add boundaries to grid

--- src/app/components/GridLogic/GridLogic.tsx
+++ src/app/components/GridLogic/GridLogic.tsx
@@ -4,27 +4,35 @@ import { BLOCK_MAP, BlockShape, RotationOption } from 'utils/block';
 import { SPAWN_LOCATION, SPAWN_ROTATION } from 'utils/constants';
 import { getRandomInt } from 'utils/numberUtils';
 
-interface BlockData {
-  refCoord: CellCoord;
-  shape: BlockShape;
-  rotation: RotationOption;
-}
-
 interface CellCoord {
   row: number;
   column: number;
+}
+
+type Rotation = 0 | 1 | 2 | 3;
+
+interface BlockData {
+  refCoord: CellCoord;
+  shape: BlockShape;
+  rotation: Rotation;
 }
 
 interface PositionData {
   [key: number]: { [key: number]: BlockShape };
 }
 
-type Rotation = 0 | 1 | 2 | 3;
-
 const GridLogic = ({ rows, columns }: { rows: number; columns: number }): JSX.Element => {
   const [refCoord, setRefCoord] = useState<CellCoord>(SPAWN_LOCATION);
   const [blockRotation, setBlockRotation] = useState<Rotation>(SPAWN_ROTATION);
   const [blockShape] = useState<BlockShape>(getRandomBlockShape());
+
+  const [blockPosition, setBlockPosition] = useState<PositionData>(
+    calculateBlockPosition({
+      refCoord,
+      shape: blockShape,
+      rotation: blockRotation,
+    }),
+  );
 
   const [matrix, setMatrix] = useState<Cell[][]>(
     [...(new Array(rows) as Cell[][])].map((_, row) =>
@@ -38,30 +46,17 @@ const GridLogic = ({ rows, columns }: { rows: number; columns: number }): JSX.El
 
   useEffect(() => {
     window.addEventListener('keydown', handleKeyDownEvent);
-
-    return () => {
-      window.removeEventListener('keydown', handleKeyDownEvent);
-    };
+    return () => window.removeEventListener('keydown', handleKeyDownEvent);
   }, [refCoord, blockRotation]);
 
-  useEffect(() => {
-    const rotationOptions: RotationOption[] = ['rotation-0', 'rotation-1', 'rotation-2', 'rotation-3'];
-
-    updateMatrix(
-      calculateBlockPosition({
-        refCoord,
-        shape: blockShape,
-        rotation: rotationOptions[blockRotation],
-      }),
-    );
-  }, [refCoord, blockRotation]);
+  useEffect(() => updateMatrix(blockPosition), [blockPosition]);
 
   function getRandomBlockShape(): BlockShape {
     return (Object.keys(BlockShape) as BlockShape[])[getRandomInt(7)];
   }
 
   function calculateBlockPosition({ refCoord: _refCoord, shape, rotation }: BlockData): PositionData {
-    const squares = BLOCK_MAP[shape][rotation];
+    const squares = BLOCK_MAP[shape][`rotation-${rotation}` as RotationOption];
 
     const position = squares.reduce((block, currentSquare) => {
       const row = _refCoord.row + currentSquare.row;
@@ -96,22 +91,49 @@ const GridLogic = ({ rows, columns }: { rows: number; columns: number }): JSX.El
         const min: Rotation = 0;
         const max: Rotation = 3;
 
-        setBlockRotation(blockRotation === max ? min : ((blockRotation + 1) as Rotation));
+        const updatedRotation = blockRotation === max ? min : ((blockRotation + 1) as Rotation);
+        const updatedPosition = calculateBlockPosition({
+          refCoord,
+          shape: blockShape,
+          rotation: updatedRotation,
+        });
+
+        if (hasReachedBoundary(updatedPosition)) return;
+
+        setBlockRotation(updatedRotation);
+        setBlockPosition(updatedPosition);
       },
-      ArrowDown: () => {
-        setRefCoord({ row: refCoord.row + 1, column: refCoord.column });
-      },
-      ArrowLeft: () => {
-        setRefCoord({ row: refCoord.row, column: refCoord.column - 1 });
-      },
-      ArrowRight: () => {
-        setRefCoord({ row: refCoord.row, column: refCoord.column + 1 });
-      },
+      ArrowDown: () => handleRefCoordChange({ row: refCoord.row + 1, column: refCoord.column }),
+      ArrowLeft: () => handleRefCoordChange({ row: refCoord.row, column: refCoord.column - 1 }),
+      ArrowRight: () => handleRefCoordChange({ row: refCoord.row, column: refCoord.column + 1 }),
       Space: () => {},
       none: () => {},
     };
 
     return handlers[keyCode] || handlers.none;
+  }
+
+  function handleRefCoordChange(updatedRefCoord: CellCoord) {
+    const updatedPosition = calculateBlockPosition({
+      refCoord: updatedRefCoord,
+      shape: blockShape,
+      rotation: blockRotation,
+    });
+
+    if (hasReachedBoundary(updatedPosition)) return;
+
+    setRefCoord(updatedRefCoord);
+    setBlockPosition(updatedPosition);
+  }
+
+  function hasReachedBoundary(position: PositionData): boolean {
+    const columnBoundaries = ['-1', `${columns}`];
+    const rowBoundary = `${rows}`;
+
+    return Object.entries(position).some(
+      ([row, squares]) =>
+        row === rowBoundary || Object.keys(squares).some((columnCoord) => columnBoundaries.includes(columnCoord)),
+    );
   }
 
   return <Grid height={`${rows * 24}px`} width={`${columns * 24}px`} matrix={matrix} />;
